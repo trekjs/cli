ff4deb85f1312deb5648ed71f3d7da0b6e1558c7
fundon <cfddream@gmail.com>
2015-02-03 21:48:28 +0800

    improve trek-generate

---
+++ bin/commands.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+      name: 'generate'
+    , alias: 'g'
+    , description: 'Generate new code'
+  },
+  {
+      name: 'new'
+    , alias: 'n'
+    , description: 'Create a new Trek application'
+  },
+  {
+      name: 'server'
+    , alias: 's'
+    , description: 'Start the Trek server'
+  },
+  {
+      name: 'console'
+    , alias: 'c'
+    , description: 'Start the Trek console'
+  }
+]
