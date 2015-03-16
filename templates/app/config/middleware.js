a0cc1e569e6b26cf5bc0bf6cc31a7fbc2afc222f
fundon <cfddream@gmail.com>
2015-03-16 17:45:16 +0800

    examples: update

--- templates/app/config/middleware.js
+++ templates/app/config/middleware.js
@@ -9,18 +9,31 @@ export default (app) => {
    * See `trek/defaultStack.js`
    *
    *  - logger
+   *     priority: 10
    *  - morgan
+   *     priority: 20
    *  - responseTime
+   *     priority: 30
    *  - xRequestId
+   *     priority: 40
    *  - staticCache
+   *     priority: 50
    *  - methodoverride
+   *     priority: 60
    *  - qs
+   *     priority: 70
    *  - bodyparser
+   *     priority: 80
    *  - compress
+   *     priority: 90
    *  - conditionalGet
+   *     priority: 100
    *  - etag
+   *     priority: 110
    *  - genericSession
+   *     priority: 120
    *  - router
+   *     priority: 1024
    *
    * Below middlewares, install them if you need.
    *
